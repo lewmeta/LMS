75160e23f66e577e22df8bb09ffd7b078410834b
thegypsyboyo <lewisonyango9@gmail.com>
2024-07-22 17:09:16 +0300

    slight navbar

---
+++ app/(dashboard)/_components/mobile-sidebar.tsx
@@ -0,0 +1,11 @@
+import { Menu } from "lucide-react"
+
+export const MobileSibar = () => {
+    return (
+        <div>
+            <Menu
+                size={22}
+            />
+        </div>
+    )
+}
