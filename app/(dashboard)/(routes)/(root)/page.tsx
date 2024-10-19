6609d29af2ebd3536b74d4cfc7ce3bedb50f3212
lewmeta <dev.lewismeta@gmail.com>
2024-10-19 10:59:03 +0300

    updated banner card and info card components

--- app/(dashboard)/(routes)/(root)/page.tsx
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -4,18 +4,49 @@ import { CheckCircle, Clock, InfoIcon } from "lucide-react";
 
 import { getDashboardCourses } from "@/actions/get-dashboard-courses";
 import { CourseList } from "@/components/course-list";
+import { BannerCard } from "./_components/banner-card";
+import { InfoCard } from "./_components/info-card";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/")
+  }
+
+  const { completedCourses, coursesInProgress } = await getDashboardCourses(userId);
+
   return (
     <div className="p-6 space-y-4">
 
       <div className="grid grid-cols-1 gap-4">
-        Banner here
+        <BannerCard
+          icon={InfoIcon}
+          label="Welcome to the dashboard"
+          description={`This is where you can see your progress 
+            and continue your courses. This is a demonstration LMS and as such, all courses are free and Stripe is in test
+             mode. To enroll in a course, enter dummy data in the Stripe form. Contact me from
+             folio.kendev.co to obtain admin access`}
+        />
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2">
-        Card goes here
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <InfoCard
+          icon={Clock}
+          label="In Progress"
+          numberOfItems={coursesInProgress.length}
+        />
+        <InfoCard
+          icon={CheckCircle}
+          label="Completed"
+          numberOfItems={completedCourses.length}
+          variant="success"
+        />
       </div>
+      <CourseList
+        items={[...coursesInProgress, ...completedCourses]}
+      />
     </div>
   );
+
 }
